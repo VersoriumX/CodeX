6178690963aa7ecbec3f07ae4d849798a013c27a
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-03-04 19:09:08 -0500

    Create customtokenpattern.js
    Signed-off-by: Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>

---
+++ public/customtokenpattern.js
@@ -0,0 +1,8 @@
+// customTokenPatterns.js
+
+export const customTokenPatterns = [
+    // Match the token names with ANSI escape codes
+    '\\u001B\\[32methereumx\\u001B\\[0m', // Green text for ethereumx
+    '\\u001B\\[34mneomatix\\u001B\\[0m',  // Blue text for neomatix
+    '\\u001B\\[35mlyrix\\u001B\\[0m'      // Magenta text for lyrix
+];
