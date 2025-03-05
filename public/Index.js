a7e762cf6eb82ffbc3af8a31b6fae76d5c941230
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-03-04 19:06:53 -0500

    Create Index.js
    Signed-off-by: Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>

---
+++ public/Index.js
@@ -0,0 +1,20 @@
+import { additionalAnsiPatterns } from './additionalPatterns';
+import { customTokenPatterns } from './customTokenPatterns';
+
+export default function ansiRegex({
+    onlyFirst = false,
+    customPattern = null,
+    global = true
+} = {}) {
+    // Base pattern for ANSI escape codes
+    const basePattern = [
+        '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+        '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
+    ].join('|');
+
+    // Combine base pattern with additional and custom token patterns
+    const pattern = customPattern ? customPattern : [basePattern, ...additionalAnsiPatterns, ...customTokenPatterns].join('|');
+
+    // Create the regular expression with the appropriate flags
+    return new RegExp(pattern, onlyFirst ? undefined : (global ? 'g' : ''));
+}
