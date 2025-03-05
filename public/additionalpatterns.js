3543340473095a0c876fb3eebf6f950974ee1b4c
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-03-04 19:08:16 -0500

    Create additionalpatterns.js
    Signed-off-by: Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>

---
+++ public/additionalpatterns.js
@@ -0,0 +1,12 @@
+// additionalPatterns.js
+
+export const additionalAnsiPatterns = [
+    // Example additional ANSI patterns
+    '\\u001B\\[0m', // Reset
+    '\\u001B\\[1m', // Bold
+    '\\u001B\\[3m', // Italic
+    '\\u001B\\[4m', // Underline
+    '\\u001B\\[7m'  // Inverse
+];
+
+"C:\Users\mace\AppData\Local\node\corepack\v1\npm\10.8.3\node_modules\@VersoriumX\cliui\node_modules"
